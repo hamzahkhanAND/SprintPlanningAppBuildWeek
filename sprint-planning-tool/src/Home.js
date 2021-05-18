6d6e5c944c1f18e4422572565c721d770a4f5866
Kyle Calbert <kylecalbert@Kyles-MBP.lan>
2021-05-18 16:08:29 +0100

    kyle commit homepage

---
+++ sprint-planning-tool/src/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className = "home-container">
+
+       <div className = "content-container">
+        <div clasName="sign-up-btns">
+            <button>Log in</button>  
+            <button>Sign Up</button>   
+ 
+
+
+
+
+
+        </div>
+       </div>
+        
+            
+        </div>
+    )
+}
+
+export default Home
