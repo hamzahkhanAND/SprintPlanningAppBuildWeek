a3e85d0031d6994091e357cdbc21a676b79c22c8
Sukhvir Notta <sukhvirnotta@sukhvirs-mbp.home>
2021-05-19 15:16:45 +0100

    Updated button on display name page

--- sprint-planning-tool/src/DisplayName.js
+++ sprint-planning-tool/src/DisplayName.js
@@ -12,7 +12,7 @@ function DisplayName() {
                     <input type="text" name="displayName" placeholder="Display name" className="rounded p-3 border w-3/4 lg:w-1/4" />
                 </div>
                 <div className="my-9">
-                    <input type="submit" value="Let the race begin" className="rounded-full bg-yellow-500 text-white text-2xl md:text-3xl  font-semibold py-3 px-9 w-3/4 lg:w-1/4" />
+                    <input type="submit" value="Let the race begin" className="rounded-full bg-yellow-500 text-white text-2xl font-semibold py-3 px-9 w-3/4 lg:w-1/4" />
                 </div>
             </form>
         </div>
