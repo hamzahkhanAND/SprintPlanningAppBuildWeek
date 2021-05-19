176349927c6105d997795ef228cd41978f39ba5e
Hamzah Khan <hamzahkhan@Hamzahs-MacBook-Pro.local>
2021-05-19 11:22:48 +0100

    Firebase set up complete

--- sprint-planning-tool/src/App.js
+++ sprint-planning-tool/src/App.js
@@ -1,5 +1,21 @@
 import './App.css';
 import Home from './Home'
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7EVtMCk59p7HU_iQmj6EJydWrhB9zwb4",
+  authDomain: "sprint-planning-tool-and.firebaseapp.com",
+  projectId: "sprint-planning-tool-and",
+  storageBucket: "sprint-planning-tool-and.appspot.com",
+  messagingSenderId: "1048128510912",
+  appId: "1:1048128510912:web:bc809f04c6482ea4d5c625",
+  measurementId: "G-228SL41M9E"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
