4f0591cdaf5b7b70fb7dd37abc9fdfd1cd6f5c46
Kyle Calbert <kylecalbert@Kyles-MBP.lan>
2021-05-19 09:39:34 +0100

    linked start button to setup page

--- sprint-planning-tool/src/App.js
+++ sprint-planning-tool/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Home from './Home'
+import Setup from './Setup'
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+
 function App() {
   return (
+    <Router>
     <div className="App">
-    <Home/>
-    
+      <Switch>
+
+ <Route path='/' exact component={Home}/>
+
+<Route path='/setup' component={Setup}/>
+
+    </Switch>
+ 
     </div>
-  );
+    </Router>
+  ); 
 }
 
 export default App;
