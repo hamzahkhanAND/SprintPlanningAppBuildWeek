898f2a041126156b275f6fa53759fac00e38bf02
Hamzah Khan <hamzahkhan@Hamzahs-MacBook-Pro.local>
2021-05-21 10:49:21 +0100

    Players can join by PIN

--- sprint-planning-tool/src/JoinGame.js
+++ sprint-planning-tool/src/JoinGame.js
@@ -1,18 +1,35 @@
-import React from 'react'
 
-function JoinGame() {
+import React, { useState } from 'react'
+
+function JoinGame(props) {
+    const [gamePIN, setGamePIN] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+
+        props.history.push({
+            pathname: '/displayName',
+            state: {
+                value : gamePIN // pass the extracted url params here
+                 } // your data array of objects
+          })
+
+          console.log(gamePIN)
+            
+    }
     return (
         <div className="container mx-auto">
         <div className="flex items-center justify-center text-center h-screen">
-            <form>
+            <form onSubmit = {handleSubmit}>
                 <div className="my-9">
                     <label className="text-3xl md:text-5xl font-semibold">Enter Game PIN</label>
                 </div>
                 <div className="my-9">
-                    <input type="text" name="gamePIN" placeholder="Game PIN" className="rounded py-4 px-3 border w-3/4" />
+                    <input type="text" name="gamePIN" placeholder="Game PIN" className="rounded py-4 px-3 border w-3/4" value ={gamePIN}
+                        onChange={(e) => setGamePIN(e.target.value)} />
                 </div>
                 <div className="my-9">
-                    <input type="submit" value="Join Game" className="rounded-full bg-yellow-500 text-white text-2xl font-semibold py-4 px-9 w-3/4 cursor-pointer" />
+                    <input type="submit" value="Join Game" className="rounded-full bg-yellow-500 text-white text-2xl font-semibold py-4 px-9 w-3/4 cursor-pointer"  />
                 </div>
             </form>
         </div>
