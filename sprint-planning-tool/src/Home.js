fd06611dee0ef7e9b02239fa73aac31f84fdf9ad
Sukhvir Notta <sukhvirnotta@sukhvirs-mbp.home>
2021-06-15 11:58:22 +0100

    Removed whitespace from homepage

--- sprint-planning-tool/src/Home.js
+++ sprint-planning-tool/src/Home.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 function Home() {
   return (
-    <div className="container mx auto flex bg-yellow-200 h-screen w-screen sm:max-h-screen sm:max-w-screen">
+    <div className="mx-auto flex bg-yellow-200 h-screen w-screen sm:max-h-screen sm:max-w-screen">
       <div className="bg-yellow-200 flex sm:flex-row sm:items-center sm:justify-center w-screen flex-col justify-center space-y-4">
-        <div className="w-auto h-auto flex flex-col items-center ml-16 w-8 sm:w-auto ">
+        <div className="w-auto h-auto flex flex-col items-center ml-16 sm:w-auto ">
           <div className=" container w-96 h-auto ">
             <h1 className=" text-4xl md:text-6xl font-bold break-words">
               Sprint To the end
